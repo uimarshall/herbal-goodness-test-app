af83d3c8287e8fdbc19be19acce8c7198d10d6e7
uimarshall <uimarshall@gmail.com>
2020-04-22 02:19:04 +0100

    add controllers for signup and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,54 @@
+const HttpStatus = require("http-status-codes");
+const StatusText = require("../utils/constants");
+const User = require("../models/User");
+
+const { ERROR, FAIL, SUCCESS } = StatusText;
+const { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR } = HttpStatus;
+
 // @desc Register users
-// @ route POST /api/users/register
+// @ route POST /api/users/signup
 // @access Public
+exports.signup = async (req, res) => {
+	try {
+		// Create new user
+		const newUser = await new User(req.body);
+		console.log("req.body", req.body);
+		const userCreated = await newUser.save();
+		return res.status(CREATED).json({
+			// The userCreated is an object
+			data: userCreated,
+			status: SUCCESS,
+		});
+	} catch (err) {
+		if (err) {
+			return res.status(BAD_REQUEST).json({
+				message: HttpStatus.getStatusText(BAD_REQUEST),
+				status: FAIL,
+			});
+		}
+	}
+};
+
+// @desc Login users
+// @ route POST /api/users/login
+// @access Public
+exports.signin = async (req, res) => {
+	// Find user by email
+
+	try {
+		const { email, password } = req.body;
+		await User.findOne({ email }, (err, userFound) => {
+			if (err || !userFound) {
+				return res.status(BAD_REQUEST).json({
+					message: `User with this ${email} does not exist`,
+				});
+			}
+			return res.json({ userFound });
+		});
+	} catch (error) {
+		return res.status(INTERNAL_SERVER_ERROR).send({
+			message: HttpStatus.getStatusText(INTERNAL_SERVER_ERROR),
+			status: FAIL,
+		});
+	}
+};
