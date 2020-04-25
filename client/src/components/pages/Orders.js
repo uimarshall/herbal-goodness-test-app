ee0affd4022e17c6dc0998b3b66b608b62386c08
uimarshall <uimarshall@gmail.com>
2020-04-25 16:26:57 +0100

    integrate API call

--- client/src/components/pages/Orders.js
+++ client/src/components/pages/Orders.js
@@ -1,36 +1,83 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Preloader from "../utils/Preloader";
 
-class Orders extends Component {
-	render() {
-		return (
-			<div className="container order">
-				<div className="row">
-					<div className="col-md-6">
-						<h5 className="text-right text-danger">Orders Details:</h5>
-					</div>
+const Orders = () => {
+	const [orderlist, setOrders] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const url =
+		"https://td8ton72u4.execute-api.us-east-2.amazonaws.com/test/orders";
+	const fetchData = async () => {
+		try {
+			// While waiting for the data to be fetched
+			setLoading(true);
+			// Make call to API
+			const result = await axios.get(url);
 
-					<table className="table table-striped">
-						<thead className="thead-light">
-							<tr>
-								<th>Name of Customer</th>
-								<th>Product Ordered</th>
-								<th> Quantity</th>
-								<th> Date</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>Omega B</td>
-								<td>Celgevity</td>
-								<td>Vitabiotics</td>
-								<td>Covid-19 cure</td>
-							</tr>
-						</tbody>
-					</table>
+			const ordersFound = result.data;
+
+			setOrders(ordersFound.orders);
+			setLoading(false);
+		} catch (error) {
+			if (error) {
+				console.log(error);
+			}
+		}
+	};
+	// const fetchData = async () => {
+	// 	try {
+	// 		setLoading(true);
+	// 		let result = await fetch(
+	// 			"https://td8ton72u4.execute-api.us-east-2.amazonaws.com/test/orders"
+	// 		);
+
+	// 		const data = await result.json();
+	// 		await setOrders(data.orders);
+	// 		setLoading(false);
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
+	useEffect(() => {
+		fetchData();
+	});
+
+	const showLoading = () => (loading ? <Preloader /> : "");
+	const showOrders = () => {
+		console.log(orderlist.gateway);
+		return orderlist.map((eachOrder, index) => (
+			<tr key={index}>
+				<td>
+					{eachOrder.customer.first_name} {eachOrder.customer.last_name}
+				</td>
+				<td>{eachOrder.name}</td>
+				<td>{eachOrder.line_items.quantity}</td>
+				<td>{eachOrder.created_at}</td>
+			</tr>
+		));
+	};
+
+	return (
+		<div className="container order order-menu">
+			<div className="row">
+				<div className="col-md-6">
+					<h5 className="text-right text-danger">Orders Details:</h5>
 				</div>
+				{showLoading()}
+				<table className="table table-striped">
+					<thead className="thead-light">
+						<tr>
+							<th>Name of Customer</th>
+							<th>Product Ordered</th>
+							<th> Quantity</th>
+							<th> Date</th>
+						</tr>
+					</thead>
+					<tbody>{showOrders()}</tbody>
+				</table>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Orders;
