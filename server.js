53d53aac08ce79c1622f75196672c4e0d29fa387
uimarshall <uimarshall@gmail.com>
2020-04-25 18:02:11 +0100

    prepared for deployment

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ app.use(cors());
 
 // Routes
 app.use("/api/users", routes);
+// Serve static assets if in Production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
